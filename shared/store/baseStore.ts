273e6ec19ed4ef74ce06da68d2517a79f9b86855
kostalakosta <k.kostalakosta@yandex.ru>
2024-07-04 22:54:54 +0300

    fix types

--- shared/store/baseStore.ts
+++ shared/store/baseStore.ts
@@ -2,11 +2,12 @@ import { create } from 'zustand';
 export const useBaseStore = create((set) => ({
   tg: null,
   userId: null,
-  setTg: (tg: TGUser) =>
+  setTg: (tg: any) =>
     set(() => ({
       tg: tg,
       userId: tg?.initDataUnsafe?.user?.id,
     })),
+
   removeTg: () => set({ tg: null }),
   removeUserId: () => set({ userId: null }),
 }));
